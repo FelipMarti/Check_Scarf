0809f1d419d9e82f8a014918afd93aa1df1fedac
Felip Marti <felip.marti.carrillo@gmail.com>
2014-09-09 12:04:26 +0200

    Output of the main function modified, example updated

--- src/examples/check_scarf_test.cpp
+++ src/examples/check_scarf_test.cpp
@@ -4,32 +4,67 @@
 int main(int argc, char *argv[])
 {
 
-	std::
-	    cout << "This is an example of how to check the scarf" << std::endl;
 	// Create a CheckScarf class;
 	CheckScarf c;
 
 	while (true) {
 
-		// Check and get values
+		// Output var is a vector
 		std::vector < int >v;
+		// Check scarf 
 		c.check_scarf(v);
 
+		// Understanding the output
 		if (v[0] < 0) {
-			std::cout << "Something wrong happen. Error code: " <<
-			    v[0] << std::endl;
-		}
-		else {
-			std::cout << "the distance Mark-Scarf is: " << v[0] <<
-			    "px" << std::endl;
-			if (v[1])
-				std::cout << "The Scarf is arround the neck" <<
+			std::cout << "Error code: " << v[0] << std::endl;
+			if (v[0] == -2)
+				std::cout <<
+				    "Neither Marker nor Scarf are found" <<
 				    std::endl;
-			else
-				std::cout << "The Scarf is NOT arround the neck"
-				    << std::endl;
+			if (v[0] == -3)
+				std::cout << "Marker not found" << std::endl;
+			if (v[0] == -4)
+				std::cout << "Scarf not found" << std::endl;
+
+			std::cout << std::endl;
 
 		}
+		else {
+			if (v[1]) {
+				std::cout << "The Scarf is around the neck at: "
+				    << v[2] << "px" << std::endl;
+			}
+			else {
+				std::cout << "The Scarf is NOT around the neck"
+				    << std::endl;
+			}
+
+			if (v[3]) {
+				std::cout <<
+				    "The Scarf end is hanging on left side at: "
+				    << v[4] << "px" << std::endl;
+			}
+			else {
+				std::cout <<
+				    "The Scarf is NOT hanging on left side" <<
+				    std::endl;
+			}
+
+			if (v[5]) {
+				std::cout <<
+				    "The Scarf end is hanging on right side at: "
+				    << v[6] << "px" << std::endl;
+			}
+			else {
+				std::cout <<
+				    "The Scarf is NOT hanging on right side" <<
+				    std::endl;
+			}
+
+			std::cout << std::endl;
+
+		}
+
 		// Show images
 		c.draw_info();
 
